5f4cca21031303668a9d81921b83ecc4a7c57aac
vampkk <alexsoren667@gmail.com>
2022-08-22 12:27:50 +0300

    Update Section 4

---
+++ API/Extensions/SwaggerServiceExtensions.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.OpenApi.Models;
+
+namespace API.Extensions
+{
+    public static class SwaggerServiceExtensions
+    {
+        public static IServiceCollection AddSwaggerServices(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
+            });
+
+            return services;
+        }
+    }
+}
