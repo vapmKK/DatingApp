a8cca3bbbd7c022398dbb71a99dca0af319ad0a4
vampkk <alexsoren667@gmail.com>
2022-08-27 21:16:42 +0300

    UPDATE 7

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -9,10 +9,9 @@ namespace API.DTOs
     public class RegisterDto
     {
         [Required]
-
         public string Username { get; set; }
         [Required]
-        
+        [StringLength(8, MinimumLength = 3)]
         public string Password { get; set; }
     }
 }
