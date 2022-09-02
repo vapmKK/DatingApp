33322a0ccb0c2e9413fa7b7d99330a87549b1f46
vampkk <alexsoren667@gmail.com>
2022-09-02 19:22:06 +0300

    Update 12 (Update to Reactive Forms)

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -7,9 +7,14 @@ using System.ComponentModel.DataAnnotations;
 namespace API.DTOs
 {
     public class RegisterDto
-    {
-        [Required]
-        public string Username { get; set; }
+    { 
+        [Required] public string Username { get; set; }
+        [Required] public string KnowAs { get; set; }
+        [Required] public string gender { get; set; }
+        [Required] public DateTime DateOfBirth { get; set; }
+        [Required] public string City { get; set; }
+        [Required] public string Country { get; set; }
+
         [Required]
         [StringLength(8, MinimumLength = 3)]
         public string Password { get; set; }
